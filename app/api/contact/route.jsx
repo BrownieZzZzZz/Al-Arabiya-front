b8ce6cf97e6e6669f756b29bcbd892b55c0c44b0
Jawhar Hmidi <jawharhmidi02@gmail.com>
2024-12-28 12:25:35 +0100

    .

---
+++ app/api/contact/route.jsx
@@ -0,0 +1,75 @@
+import { NextResponse } from "next/server";
+const nodemailer = require("nodemailer");
+
+export async function POST(request) {
+  const receiver = process.env.NEXT_PUBLIC_EMAIL_RECEIVER;
+  const username = process.env.NEXT_PUBLIC_EMAIL_USERNAME;
+  const password = process.env.NEXT_PUBLIC_EMAIL_PASSWORD;
+
+  const formData = await request.formData();
+
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const phone = formData.get("phone");
+  const message = formData.get("message");
+
+  // Create transporter object
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: username,
+      pass: password,
+    },
+  });
+
+  try {
+    const mail = await transporter.sendMail({
+      from: username,
+      to: receiver,
+      subject: `طلب اتصال جديد: ${name}`,
+      html: `<div style="font-family: Arial, sans-serif; color: #2f2f2f; padding: 20px; background-color: #f7f6f1; direction: rtl;">
+          <div style="max-width: 600px; margin: auto; background-color: #fff; border-radius: 10px; overflow: hidden; box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);">
+            <header style="background-color: #d99814; padding: 20px; text-align: center; color: #fff;">
+              <h3 style="margin: 0; font-size: 24px;">طلب اتصال جديد</h3>
+              <p style="font-size: 16px; margin: 5px 0;">
+                لقد تلقيت رسالة من موقعك الإلكتروني
+              </p>
+            </header>
+
+            <section style="padding: 20px;">
+              <p style="font-size: 18px; line-height: 1.6; margin: 10px 0;">
+                <strong>الاسم:</strong> ${name}
+              </p>
+              <p style="font-size: 18px; line-height: 1.6; margin: 10px 0;">
+                <strong>البريد الإلكتروني:</strong> ${email}
+              </p>
+              <p style="font-size: 18px; line-height: 1.6; margin: 10px 0;">
+                <strong>رقم الهاتف:</strong> ${phone}
+              </p>
+              <p style="font-size: 18px; line-height: 1.6; margin: 20px 0 10px;">
+                <strong>الرسالة:</strong>
+              </p>
+              <div style="padding: 15px; background-color: #f7f6f1; border-right: 4px solid #d99814; font-size: 16px; line-height: 1.8;">
+                ${message}
+              </div>
+            </section>
+
+            <footer style="background-color: #d99814; padding: 15px; text-align: center; color: #fff;">
+              <p style="margin: 0; font-size: 14px;">
+                شكراً لاستخدامك خدمتنا! لمزيد من المعلومات، يرجى زيارة موقعنا
+                الإلكتروني.
+              </p>
+              <p style="margin: 0; font-size: 14px;">مع تحيات، شركة العربية</p>
+            </footer>
+          </div>
+        </div>`,
+    });
+
+    return NextResponse.json({ message: "تم إرسال البريد الإلكتروني بنجاح" });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.status(500).json({
+      message: "تعذر إرسال الرسالة",
+    });
+  }
+}
--- app/contact/page.js
+++ app/contact/page.js
@@ -1,81 +1,225 @@
-import React from "react";
-import { FaTiktok } from "react-icons/fa";
+"use client";
+
+import { toast } from "@/hooks/use-toast";
+import { cn } from "@/lib/utils";
+import { useRef, useState } from "react";
 
 const page = () => {
+  const phoneRef = useRef(null);
+  const [loading, setLoading] = useState(false);
+
+  const validateNumberInput = (price) => {
+    const input = price.current;
+    let value = input.value;
+
+    const regex = /^\+{0,1}[0-9]*$/;
+
+    if (!regex.test(value)) {
+      input.value = value.slice(0, -1);
+    }
+  };
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    const name = formData.get("name");
+    const phone = formData.get("phone");
+    const email = formData.get("email");
+    const message = formData.get("message");
+
+    if (name.trim() === "") {
+      toast({
+        title: "خطأ",
+        description: "حقل الاسم مطلوب",
+        variant: "destructive",
+        duration: 2000,
+      });
+      return;
+    }
+
+    if (phone.trim() === "") {
+      toast({
+        title: "خطأ",
+        description: "حقل الهاتف مطلوب",
+        variant: "destructive",
+        duration: 2000,
+      });
+      return;
+    }
+
+    if (email.trim() === "") {
+      toast({
+        title: "خطأ",
+        description: "حقل البريد الإلكتروني مطلوب",
+        variant: "destructive",
+        duration: 2000,
+      });
+      return;
+    }
+
+    if (message.trim() === "") {
+      toast({
+        title: "خطأ",
+        description: "حقل الرسالة مطلوب",
+        variant: "destructive",
+        duration: 2000,
+      });
+      return;
+    }
+    setLoading(true);
+
+    try {
+      const response = await fetch("/api/contact", {
+        method: "post",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error(`response status: ${response.status}`);
+      }
+      await response.json();
+      toast({
+        title: "نجاح",
+        description: "تم إرسال الرسالة بنجاح",
+        variant: "success",
+        duration: 2000,
+      });
+      setLoading(false);
+    } catch (err) {
+      console.error(err);
+      toast({
+        title: "خطأ",
+        description: "تعذر إرسال الرسالة",
+        variant: "destructive",
+        duration: 2000,
+      });
+      setLoading(false);
+    }
+  }
+
   return (
     <div
       dir="rtl"
       className="mx-auto mt-20 flex w-full items-center justify-center"
     >
-      <div className="mx-6 md:mx-10 grid min-[800px]:grid-cols-2 w-full max-w-[1300px] gap-6  min-[800px]:gap-10">
+      <div className="mx-6 grid w-full max-w-[1300px] gap-6 md:mx-10 min-[800px]:grid-cols-2 min-[800px]:gap-10">
         <div className="flex w-full flex-col items-center justify-center gap-6">
-          <div className="min-[800px]:self-start min-[800px]:mr-10 mb-4 relative font-lato text-4xl font-semibold before:absolute before:left-full before:top-3 before:h-10 before:w-10 before:border-b-2 before:border-r-2 before:border-[var(--theme)] before:content-[''] after:absolute after:-left-10 after:-top-2 after:h-10 after:w-10 after:border-l-2 after:border-t-2 after:border-[var(--theme)] after:bg-transparent after:transition-all after:content-['']">
+          <div className="font-lato relative mb-4 text-4xl font-semibold before:absolute before:left-full before:top-3 before:h-10 before:w-10 before:border-b-2 before:border-r-2 before:border-[var(--theme)] before:content-[''] after:absolute after:-left-10 after:-top-2 after:h-10 after:w-10 after:border-l-2 after:border-t-2 after:border-[var(--theme)] after:bg-transparent after:transition-all after:content-[''] min-[800px]:mr-10 min-[800px]:self-start">
             <span>اتصل بنا</span>
           </div>
 
-          <div className="tracking-wider text-neutral-700 max-w-[500px] min-[800px]:self-start ">
+          <div className="max-w-[500px] tracking-wider text-neutral-700 min-[800px]:self-start">
             نحن نفتخر بتقديم خدمة دعم عملاء استثنائية تضع احتياجاتكم في المقدمة.
             فريقنا المتخصص جاهز دائمًا للإجابة على استفساراتكم وحل مشاكلكم بسرعة
             واحترافية، لضمان رضاكم التام وتعزيز ثقتكم بنا.
           </div>
 
-          <div className="grid w-full min-[550px]:w-fit min-[550px]:grid-cols-2 min-[800px]:grid-cols-1 gap-6 min-[800px]:self-start">
-            <div className="flex flex-col gap-4 rounded-xl items-center bg-white p-8">
+          <div className="grid w-full gap-6 min-[550px]:w-fit min-[550px]:grid-cols-2 min-[800px]:grid-cols-1 min-[800px]:self-start">
+            <div className="flex flex-col items-center gap-4 rounded-xl bg-white p-8">
               <div className="flex flex-col gap-4">
-              <div className="flex flex-row items-center gap-3">
-                <i className="fa-solid fa-phone-flip text-xl text-[var(--theme)]"></i>
-                <div dir="ltr">+ 216 12 345 678</div>
+                <div className="flex flex-row items-center gap-3">
+                  <i className="fa-solid fa-phone-flip text-xl text-[var(--theme)]"></i>
+                  <div dir="ltr">+ 216 12 345 678</div>
+                </div>
+                <div className="flex flex-row items-center gap-3">
+                  <i className="fa-solid fa-envelope text-xl text-[var(--theme)]"></i>
+                  <div dir="ltr">Example@gmail.com</div>
+                </div>
               </div>
-              <div className="flex flex-row items-center gap-3">
-                <i className="fa-solid fa-envelope text-xl text-[var(--theme)]"></i>
-                <div dir="ltr">Example@gmail.com</div>
-              </div>
-            </div>
             </div>
 
             <div className="flex items-center justify-center rounded-xl bg-white p-8">
               <div className="flex w-[220px] flex-col gap-5">
-                <div className="text-xl text-center font-semibold text-neutral-800">
+                <div className="text-center text-xl font-semibold text-neutral-800">
                   يمكنك أن تجدنا على
                 </div>
                 <div className="flex flex-row items-center justify-between">
                   <img
                     src="/icons/instagram.png"
                     alt="ig"
-                    className="size-[32px] hover:cursor-pointer transition-all duration-200 hover:scale-110"
+                    className="size-[32px] transition-all duration-200 hover:scale-110 hover:cursor-pointer"
                   ></img>
                   <img
                     src="/icons/facebook.png"
                     alt="ig"
-                    className="size-[32px] hover:cursor-pointer transition-all duration-200 hover:scale-110"
+                    className="size-[32px] transition-all duration-200 hover:scale-110 hover:cursor-pointer"
                   ></img>
                   <img
                     src="/icons/whatsapp.png"
                     alt="ig"
-                    className="size-[32px] hover:cursor-pointer transition-all duration-200 hover:scale-110"
+                    className="size-[32px] transition-all duration-200 hover:scale-110 hover:cursor-pointer"
                   ></img>
                   <img
                     src="/icons/tiktok.png"
                     alt="ig"
-                    className="size-[32px] hover:cursor-pointer transition-all duration-200 hover:scale-110"
+                    className="size-[32px] transition-all duration-200 hover:scale-110 hover:cursor-pointer"
                   ></img>
                 </div>
               </div>
             </div>
           </div>
-
         </div>
-
-        <div className="bg-white py-8 min-[550px]:py-12 px-5 min-[550px]:px-10 flex flex-col gap-3 rounded-xl w-full">
-          <div className="flex-col flex gap-3 sm:flex-row w-full">
-            <input dir="rtl" type="text" placeholder="إسم" className="w-full bg-[var(--theme2)] p-2.5 placeholder-neutral-400 outline-[var(--theme)]"></input>
-            <input dir="rtl" type="email" placeholder="بريد إلكتروني" className="w-full bg-[var(--theme2)] p-2.5 placeholder-neutral-400 outline-[var(--theme)]"></input>
+        <form
+          onSubmit={handleSubmit}
+          method="post"
+          role="form"
+          className="php-email-form"
+        >
+          <div className="flex w-full flex-col gap-3 rounded-xl bg-white px-5 py-8 min-[550px]:px-10 min-[550px]:py-12">
+            <div className="flex w-full flex-col gap-3 sm:flex-row">
+              <input
+                dir="rtl"
+                type="text"
+                placeholder="إسم"
+                name="name"
+                className="w-full bg-[var(--theme2)] p-2.5 placeholder-neutral-400 outline-[var(--theme)]"
+              ></input>
+              <input
+                dir="rtl"
+                type="email"
+                placeholder="بريد إلكتروني"
+                name="email"
+                className="w-full bg-[var(--theme2)] p-2.5 placeholder-neutral-400 outline-[var(--theme)]"
+              ></input>
+            </div>
+            <input
+              dir="rtl"
+              type="text"
+              ref={phoneRef}
+              placeholder="رقم الهاتف"
+              name="phone"
+              onInput={() => validateNumberInput(phoneRef)}
+              className="w-full bg-[var(--theme2)] p-2.5 placeholder-neutral-400 outline-[var(--theme)]"
+            ></input>
+            <textarea
+              dir="rtl"
+              placeholder="رسالة"
+              name="message"
+              rows={4}
+              className="h-full w-full bg-[var(--theme2)] p-2.5 placeholder-neutral-400 outline-[var(--theme)]"
+            ></textarea>
+            <button
+              type="submit"
+              disabled={loading}
+              className={cn(
+                "mt-5 w-[150px] border-2 border-transparent bg-[var(--theme)] py-3 text-xl font-semibold text-[#ffffff] outline-none transition-all duration-200",
+                loading
+                  ? "opacity-50 hover:cursor-not-allowed"
+                  : "hover:scale-[1.07] hover:border-[var(--theme)] hover:bg-white hover:text-[var(--theme)]",
+              )}
+            >
+              {loading ? (
+                <div className="flex items-center justify-center">
+                  <div className="size-6 animate-spin rounded-full border-2 border-white border-t-transparent"></div>
+                </div>
+              ) : (
+                "أرسل"
+              )}
+            </button>
           </div>
-          <input dir="rtl" type="text" placeholder="رقم الهاتف" className="w-full bg-[var(--theme2)] p-2.5 placeholder-neutral-400 outline-[var(--theme)]"></input>
-          <textarea dir="rtl" placeholder="رسالة" className="w-full h-full bg-[var(--theme2)] p-2.5 placeholder-neutral-400 outline-[var(--theme)]"></textarea>
-          <button type="button" className="transition-all duration-200 hover:scale-[1.07] mt-5 py-3 border-2 border-transparent text-[#ffffff] bg-[var(--theme)] hover:border-[var(--theme)] hover:text-[var(--theme)] hover:bg-white font-semibold text-xl  w-[150px] outline-none">أرسل</button>
-        </div>
-
+        </form>
       </div>
     </div>
   );
