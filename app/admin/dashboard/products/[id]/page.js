726a4efca132624488af9d7746c38cea77f13eac
Lafi Raed <browniesandmuffin@gmail.com>
2025-01-01 14:25:33 +0100

    .

--- app/admin/dashboard/products/[id]/page.js
+++ app/admin/dashboard/products/[id]/page.js
@@ -1,8 +1,14 @@
+"use client";
+import { useParams } from 'next/navigation';
 import React from 'react'
 
 const page = () => {
+  const params = useParams();
+  const id = params.id;
   return (
-    <div>page</div>
+    <div className="flex w-full flex-col gap-10 px-5 pt-5 md:px-0 md:pl-10 md:pt-8 lg:pl-20 lg:pt-10 pb-10">
+      
+    </div>
   )
 }
 
--- app/admin/dashboard/products/page.js
+++ app/admin/dashboard/products/page.js
@@ -17,13 +17,14 @@ const page = () => {
     {
       id: 1,
       name: "عطر زهري",
-      category: "عطور",
+      category: { name: "عطور" },
       brand: {
         name: "Sheglam",
         img: "/images/brands/sheglam.png",
       },
       img: ["/images/product1.jpg"],
-      description: "عطر شرقي فاخر بلمسة من الزهور الفواحة والتوابل. يعكس الأناقة والجاذبية في كل مرة يُستخدم.",
+      description:
+        "عطر شرقي فاخر بلمسة من الزهور الفواحة والتوابل. يعكس الأناقة والجاذبية في كل مرة يُستخدم.",
       onSold: false,
       normalSinglePrice: 100,
       soldSinglePrice: 80,
@@ -40,7 +41,8 @@ const page = () => {
         img: "/images/brands/sheglam.png",
       },
       img: ["/images/product1.jpg"],
-      description: "عطر شرقي فاخر بلمسة من الزهور الفواحة والتوابل. يعكس الأناقة والجاذبية في كل مرة يُستخدم.",
+      description:
+        "عطر شرقي فاخر بلمسة من الزهور الفواحة والتوابل. يعكس الأناقة والجاذبية في كل مرة يُستخدم.",
       onSold: false,
       normalSinglePrice: 100,
       soldSinglePrice: 80,
@@ -57,7 +59,8 @@ const page = () => {
         img: "/images/brands/sheglam.png",
       },
       img: ["/images/product1.jpg"],
-      description: "عطر شرقي فاخر بلمسة من الزهور الفواحة والتوابل. يعكس الأناقة والجاذبية في كل مرة يُستخدم.",
+      description:
+        "عطر شرقي فاخر بلمسة من الزهور الفواحة والتوابل. يعكس الأناقة والجاذبية في كل مرة يُستخدم.",
       onSold: false,
       normalSinglePrice: 100,
       soldSinglePrice: 80,
@@ -74,7 +77,8 @@ const page = () => {
         img: "/images/brands/sheglam.png",
       },
       img: ["/images/product1.jpg"],
-      description: "عطر شرقي فاخر بلمسة من الزهور الفواحة والتوابل. يعكس الأناقة والجاذبية في كل مرة يُستخدم.",
+      description:
+        "عطر شرقي فاخر بلمسة من الزهور الفواحة والتوابل. يعكس الأناقة والجاذبية في كل مرة يُستخدم.",
       onSold: false,
       normalSinglePrice: 100,
       soldSinglePrice: 80,
@@ -91,7 +95,8 @@ const page = () => {
         img: "/images/brands/sheglam.png",
       },
       img: ["/images/product1.jpg"],
-      description: "عطر شرقي فاخر بلمسة من الزهور الفواحة والتوابل. يعكس الأناقة والجاذبية في كل مرة يُستخدم.",
+      description:
+        "عطر شرقي فاخر بلمسة من الزهور الفواحة والتوابل. يعكس الأناقة والجاذبية في كل مرة يُستخدم.",
       onSold: false,
       normalSinglePrice: 100,
       soldSinglePrice: 80,
@@ -108,7 +113,8 @@ const page = () => {
         img: "/images/brands/sheglam.png",
       },
       img: ["/images/product1.jpg"],
-      description: "عطر شرقي فاخر بلمسة من الزهور الفواحة والتوابل. يعكس الأناقة والجاذبية في كل مرة يُستخدم.",
+      description:
+        "عطر شرقي فاخر بلمسة من الزهور الفواحة والتوابل. يعكس الأناقة والجاذبية في كل مرة يُستخدم.",
       onSold: true,
       normalSinglePrice: 100,
       soldSinglePrice: 80,
@@ -118,16 +124,32 @@ const page = () => {
     },
   ];
   return (
-    <div className="flex w-full flex-col gap-10 px-5 pt-5 md:px-0 md:pl-10 md:pt-8 lg:pl-20 lg:pt-10 pb-10">
+    <div className="flex w-full flex-col gap-10 px-5 pb-10 pt-5 md:px-0 md:pl-10 md:pt-8 lg:pl-20 lg:pt-10">
       <DashSearch placeholder="عطر زهر، كريم مرطب، أو يمكنك إدخال معرف المنتوج... " />
       <div className="grid w-full auto-rows-fr gap-6 min-[500px]:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-        {products.length !== 0 ? (
-          products.map((product, index) => (
-            <DashProductCard key={index} product={product} ChangeUrl={ChangeUrl} />
-          ))
-        ) : (
-          <DashNotFoundComp text="لا يوجد منتجات " />
-        )}
+
+        {/* ADD PRODUCT */}
+        <div onClick={() => {
+          ChangeUrl("/admin/dashboard/products/add");
+        }} className="flex items-center justify-center rounded-xl bg-[var(--dash-theme2)] px-4 py-5 transition-all duration-200 hover:scale-[1.02] hover:cursor-pointer hover:bg-[#2b2b36]">
+          <div className="flex flex-col items-center justify-between gap-2">
+            <div className="grid size-[40px] place-items-center rounded-full border-2 border-neutral-300 text-center text-2xl font-semibold text-neutral-300">
+              <div className="mb-1">+</div>
+            </div>
+            <div className="text-center text-xl font-semibold text-neutral-300">
+              أضف منتوج جديدة{" "}
+            </div>
+          </div>
+        </div>
+
+        {/* PRODUCTS */}
+        {products.map((product, index) => (
+          <DashProductCard
+            key={index}
+            product={product}
+            ChangeUrl={ChangeUrl}
+          />
+        ))}
       </div>
     </div>
   );
