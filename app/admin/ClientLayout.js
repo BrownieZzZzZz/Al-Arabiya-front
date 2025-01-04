013a18ccc9eeb4518f836ff3604b97d9152dd3fb
Jawhar Hmidi <jawharhmidi02@gmail.com>
2025-01-04 05:56:36 +0100

    s

--- app/admin/ClientLayout.js
+++ app/admin/ClientLayout.js
@@ -9,7 +9,7 @@ import { toast } from "@/hooks/use-toast";
 import { AuthContext } from "@/contexts/AuthContext";
 
 import DashNav from "@/components/DashNav/DashNav";
-// import NewOrder from "@/notifications/NewOrder/NewOrder";
+//import NewOrder from "@/notifications/NewOrder/NewOrder";
 
 export default function ClientLayout({ children }) {
   const router = useRouter();
